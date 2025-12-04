d17731bcc054157e2a9829da0d43fbe28c7327b5
ovi1173 <ohiduzzaman6422@gmail.com>
2025-12-05 01:28:11 +0600

    filter by category using dropdown component added

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,19 +4,36 @@ import { motion } from 'framer-motion';
 
 const Services = () => {
     const [services, setServices] = useState([]);
+    const [category,setCategory] = useState('')
+
     useEffect(() => {
-        fetch('http://localhost:3000/services')
+        fetch(`http://localhost:3000/services?category=${category}`)
             .then(res => res.json())
             .then(data => setServices(data))
             .catch(err => console.log(err));
-    }, []);
+          
+            
+    }, [category]);
+    // console.log(category);
+    
 
     return (
         <div className="px-6 md:px-20 mt-12">
+
             {/* Title */}
-            <h2 className="text-4xl font-bold text-center text-gray-800 mb-10">
+            <h2 className="text-4xl font-bold text-center text-gray-800">
                 Our Premium Services
             </h2>
+            <div className='flex justify-end mb-8'>
+                <select onChange={(e)=>setCategory(e.target.value)} defaultValue="Choose a category" className="select ">
+                    <option disabled={true}>Choose Category</option>
+                    <option value="">All</option>
+                    <option value="Pet">Pet</option>
+                    <option value="Food">Food</option>
+                    <option value="Accessories">Accessories</option>
+                    <option value="care products">Care Products</option>
+                </select>
+            </div>
 
             {/* Grid */}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
@@ -40,7 +57,7 @@ const Services = () => {
 
                             <div className='flex justify-between mt-2'>
                                 <p className='font-medium text-gray-700'>Price: ${service?.price}</p>
-                                <p className='font-medium text-gray-700'>Date: {service?.date}</p>
+                                <p className='font-medium text-gray-700'>Category: {service?.category}</p>
                             </div>
 
                             <div className="card-actions justify-end mt-4">
